034e8c0acb1519270ca3c12fc029e877f0e78a73
emtupp <emilyc.tupper@gmail.com>
2021-02-09 16:18:24 -0500

    initial elements for the contact info component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import './Contact.css';
 
 export default function Contact(props) {
-  return <div>It me!! (:</div>;
+  return (
+    <section className='contact-box'>
+      <div className='contact-title'>
+        Contact info
+      </div>
+      <div className='contact-info'>
+        <p><strong>email</strong>: text@text.com</p>
+        <p><strong>phone</strong>: 000-000-0000</p>
+        <p><strong>P.O. Box</strong>: Some adress</p>
+        <p><strong>Amazon</strong>: Some Hyperlink</p>
+        <p><strong>Etsy</strong>: Some Hyperlink</p>
+      </div>
+    </section>
+  );
 };
