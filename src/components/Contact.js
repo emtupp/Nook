af14f73cd8f7803b946cd3046ce5e1ba42e7b69d
emtupp <emilyc.tupper@gmail.com>
2021-02-09 21:51:16 -0500

    modify contact info section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,16 +4,7 @@ import './Contact.css';
 export default function Contact(props) {
   return (
     <section className='contact-box'>
-      <div className='contact-title'>
-        Contact info
-      </div>
-      <div className='contact-info'>
-        <p><strong>email</strong>: text@text.com</p>
-        <p><strong>phone</strong>: 000-000-0000</p>
-        <p><strong>P.O. Box</strong>: Some adress</p>
-        <p><strong>Amazon</strong>: Some Hyperlink</p>
-        <p><strong>Etsy</strong>: Some Hyperlink</p>
-      </div>
+      <div><strong>email us</strong>: Nookmaison@gmail.com</div>
     </section>
   );
 };
