333fc6d0c95bdc66c1b1642f847efcac1ab61cd4
emtupp <emilyc.tupper@gmail.com>
2021-02-09 22:21:48 -0500

    create simple nav with hrefs to different websites

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,6 +4,11 @@ import './Background.css';
 export default function Background(props) {
   return (
     <div className="background">
+      <div className='hrefs'>
+        <a href=""><i class="fa fa-amazon fa_custom fa-4x"></i></a>
+        <a href=""><i class="fa fa-instagram fa_custom fa-4x"></i></a>
+        <a href=""><i class="fa fa-facebook fa_custom fa-4x"></i></a>
+      </div>
       <img src={logo} alt="Logo" className='logo' />
     </div>
   );
