546ee1dedb03d0c91457dcf11168e77164a72abf
emtupp <emilyc.tupper@gmail.com>
2021-02-09 21:44:47 -0500

    create state for displaying text

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 
 import Background from './components/Background.js';
 import Story from './components/Story.js';
-// import Contact from './components/Contact.js';
+import Contact from './components/Contact.js';
 
 function App() {
   return (
     <div className="App">
       <Story />
+      <Contact />
       <Background />
-      {/* <Contact /> */}
     </div>
   );
 }
--- src/components/Story.js
+++ src/components/Story.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Story.css';
 
 export default function Story(props) {
+  const description = "Noök is a Canadian lifestyle company focused on creating a cozy atmosphere with a modern design. Our carefully curated products allow our client to express their unique personality through decor. ";
+  const story = "Noök was founded in Montreal, Quebec by a close-knit team whose members shared a common vision: providing beautiful, high-quality, affordable products for the home. It is our belief that creating a warm, lovely home should be accessible to everyone.";
+  const [ text, setText ] = useState(description)
+
   return (
     <section className='story'>
-      <div className='description'>Noök is a Canadian lifestyle company focused on creating a cozy atmosphere with a modern design. Our carefully curated products allow our client to express their unique personality through decor. </div>
+      <div className='text'>{text}</div>
       <div className='navigate'>
-        <button className='to-description'>About us</button>
-        <button className='to-story'>Our story</button>
+        <button className='to-description' onClick={() => setText(description)}>About us</button>
+        <button className='to-story' onClick={() => setText(story)}>Our story</button>
         <button className='to-products' disabled>Pruducts</button>
       </div>
     </section>
